2068c0caca66b74f578138e2a86f39482951f061
GU <945893335@qq.com>
2018-07-19 21:51:46 +0800

    基础组件划分

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -9,15 +9,14 @@
 export default {
 }
 </script>
-<style scoped lang="stylus" rel="stylesheet/stylus">
-    import './common/stylus/variable'
-    import './common/stylus/mixin'
+<style scoped lang="less">
+    @import '../../common/less/variable.less';
 
     .m-header{
         position: relative;
         height: 44px;
         text-align: center;
-        color: $color-theme;
+        color: #999;
         font-size :0;
         .icon{
             display :inline-block;
@@ -26,24 +25,24 @@ export default {
             width:30px;
             height:32px;
             margin-right: 9px;
-            bg-image('logo');
+            background-image :url('logo.png');
             background-size:30px 32px;
         }
         .text{
             display :inline-block;
             vertical-align :top;
             line-height :44px;
-            font-size: $font-size-large;
+            font-size: 28px;
         }
         .mine{
-            position: absolute
-            top: 0
-            right: 0
+            position: absolute;
+            top: 0;
+            right: 0;
             .icon-mine{
-                display: block
-                padding: 12px
-                font-size: 20px
-                color: $color-theme
+                display: block;
+                padding: 12px;
+                font-size: 20px;
+                color: #999;
             }
         }
     }
